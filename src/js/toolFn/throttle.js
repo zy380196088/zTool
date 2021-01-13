d5d7cfe6873a8045612c9e51d92abee80fc39974
Joy <380196088@qq.com>
2021-01-13 21:34:19 +0800

    formatDataToTree

--- src/js/toolFn/throttle.js
+++ src/js/toolFn/throttle.js
@@ -1,17 +1,16 @@
 // 函数节流
-function _throttle(methods,time) {
-   var timer = null;
-   var _start_time  = new  Data();
-   return function () {
-     var context = this;
-     var _end_time = new Data();
-     var _during_time = _end_time - _start_time;
-     
-     if(_during_time >= time){
-       //大于等于设定的时间采取执行函数;       
-       methods.call(context);
-       //执行完后重置开始时间
-       _start_time = _end_time;
-       }
-     }
+function _throttle(methods, time) {
+	let timer = null;
+	let _start_time = new Date();
+	return function () {
+		let context = this;
+		let _end_time = new Date();
+		let _during_time = _end_time - _start_time;
+		if (_during_time >= time) {
+			//大于等于设定的时间采取执行函数;
+			methods.call(context);
+			//执行完后重置开始时间
+			_start_time = _end_time;
+		}
+	}
 }
